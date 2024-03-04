5d6c930e405012948e0cd606596dcfc54c3cd703
privero <65060696+p-rivero@users.noreply.github.com>
2024-03-04 14:42:19 +0100

    Fix parser errors

--- sample-node/index.js
+++ sample-node/index.js
@@ -1,8 +1,9 @@
 import { Unix } from "unix-core"
 
 // Don't forget to run `npm run bundle` to generate the dist directory
-import virtualFS from "../dist/filesystem.js"
+import virtualFS from "../dist/filesystem.cjs"
 
 const unixJs = new Unix(virtualFS)
+process.on("SIGINT", () => unixJs.interrupt())
 await unixJs.start()
 
